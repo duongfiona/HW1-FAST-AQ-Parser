882af6551bbd2a35daede1bd08d2e4409d61b9ce
Fiona Duong <fiona.duong2@ucsf.edu>
2026-01-10 14:16:14 -0800

    Implementing transcribe and reverse_transcribe in seq.py

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -10,13 +10,24 @@ def transcribe(seq: str, reverse: bool = False) -> str:
     Write a function that will transcribe (replace DNA sequence to RNA
     by replacing all 'T' to 'U') in an input sequence
     """
-    pass
+    # raising ValueError if input seq contains anything besides A, C, T, G
+    if not all(base in ALLOWED_NUC for base in seq):
+        raise ValueError("Input sequence string contains invalid nucleotides")
+
+    # iteratively transcribing input seq using TRANSCRIPTION_MAPPING dict
+    transcript = ""
+    for base in seq:
+        transcript += TRANSCRIPTION_MAPPING[base]
+
+    # if reverse=True, reverse the order of complement transcript
+    if reverse:
+        transcript = transcript[::-1]
+
+    return transcript
 
 def reverse_transcribe(seq: str) -> str:
     """
     Write a function that will transcribe an input sequence and reverse
     the sequence
     """
-    # Hey this is my comment
-    # Again!
-    pass
+    return transcribe(seq, reverse=True)
